1a3452158db3ce4aae0fc5878cb90aaf3b822883
PS Ad <78262269+aditya3284@users.noreply.github.com>
2024-05-13 23:34:48 +0530

    add initial home layout, fonts & constants

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
-function App() {
-  return <h1 className=" text-7xl font-bold underline">Hello world!</h1>;
-}
+const App = () => {
+	return (
+		<>
+			Navbar
+			<main className='relative'>
+				<section className='padding bg-black text-white'>Hero Section</section>
+				<section className='padding bg-gray-500'>Popular Products</section>
+				<section className='padding bg-blue-300'>Services</section>
+				<section className='padding bg-p-6'>Special Offer</section>
+				<section className='padding bg-s-3'>customer Review</section>
+				<section className='padding'>Newsletter</section>
+				<section className='padding bg-black text-white'>footer</section>
+			</main>
+		</>
+	);
+};
 
 export default App;
---
+++ src/constants/index.js
@@ -0,0 +1,69 @@
+const navigation = [
+	{
+		id: "0",
+		title: "Features",
+		url: "#features",
+	},
+	{
+		id: "1",
+		title: "Pricing",
+		url: "#pricing",
+	},
+	{
+		id: "2",
+		title: "How to use",
+		url: "#how-to-use",
+	},
+	{
+		id: "3",
+		title: "Roadmap",
+		url: "#roadmap",
+	},
+	{
+		id: "4",
+		title: "New account",
+		url: "#signup",
+		onlyMobile: true,
+	},
+	{
+		id: "5",
+		title: "Sign in",
+		url: "#login",
+		onlyMobile: true,
+	},
+];
+
+const socials = [
+	{
+		id: "0",
+		title: "Discord",
+		iconUrl: "",
+		url: "#",
+	},
+	{
+		id: "1",
+		title: "Twitter",
+		iconUrl: "",
+		url: "#",
+	},
+	{
+		id: "2",
+		title: "Instagram",
+		iconUrl: "",
+		url: "#",
+	},
+	{
+		id: "3",
+		title: "Telegram",
+		iconUrl: "",
+		url: "#",
+	},
+	{
+		id: "4",
+		title: "Facebook",
+		iconUrl: "",
+		url: "#",
+	},
+];
+
+export { navigation, socials };
