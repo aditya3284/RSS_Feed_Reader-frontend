bc23e450e68637060f4c16aebf2099eba2746b86
PS Ad <78262269+aditya3284@users.noreply.github.com>
2024-05-14 17:31:13 +0530

    add responsive Navbar - with implementation for mobile navbar

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,27 @@ const App = () => {
 		<>
 			<Navbar />
 			<main className='relative'>
-				<section className='padding bg-black text-white'>Hero Section</section>
-				<section className='padding bg-gray-500'>Popular Products</section>
-				<section className='padding bg-blue-300'>Services</section>
-				<section className='padding bg-p-6'>Special Offer</section>
-				<section className='padding bg-s-3'>customer Review</section>
-				<section className='padding'>Newsletter</section>
-				<section className='padding bg-black text-white'>footer</section>
+				<section id='signup' className='padding bg-black text-white'>
+					Hero Section
+				</section>
+				<section id='features' className='padding bg-gray-500'>
+					Popular Products
+				</section>
+				<section id='services' className='padding bg-blue-300'>
+					Services
+				</section>
+				<section id='pricing' className='padding bg-p-6'>
+					Special Offer
+				</section>
+				<section id='offer' className='padding bg-s-3'>
+					customer Review
+				</section>
+				<section id='how-to-use' className='padding'>
+					Newsletter
+				</section>
+				<section id='roadmap' className='padding bg-black text-white'>
+					footer
+				</section>
 			</main>
 		</>
 	);
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,44 @@
+import { useState } from "react";
 import { navigation } from "../constants";
 import MaxWidthContainer from "./MaxWidthContainer";
 
 const Navbar = () => {
+	const [openNavigation, setOpenNavigation] = useState(false);
+	const handleMenuBtnClick = () => {
+		if (openNavigation) {
+			setOpenNavigation(false);
+		} else {
+			setOpenNavigation(true);
+		}
+	};
+	const toggleNavigation = () => {
+		if (!openNavigation) return;
+		setOpenNavigation(false);
+	};
 	return (
-		<header className='border-s-6 bg-s-8/90 fixed inset-x-0 top-0 z-10 h-14 w-full border-b text-white backdrop-blur-lg transition-all'>
+		<header className='fixed inset-x-0 top-0 z-10 h-14 w-full border-b border-s-6 bg-s-8/90 text-white backdrop-blur-lg transition-all'>
 			<MaxWidthContainer>
 				<div className='font-so flex h-full items-center justify-between'>
-					<a href='/' className='font-sora z-40 font-bold'>
+					<a href='/' className='z-20 font-sora font-bold'>
 						feed<span className='bg-cyan-600'>reader</span>
 					</a>
-					<nav className='hidden md:flex'>
-						<ol className='flex items-center gap-6'>
+					<nav
+						className={`md:flex ${openNavigation ? "fixed inset-x-0 inset-y-full z-10 h-svh w-svw bg-s-5/50 backdrop-blur-sm" : "hidden"}`}
+					>
+						<ol
+							className={`flex items-center gap-6 ${openNavigation ? "h-full w-full flex-col justify-center" : ""}`}
+						>
 							{navigation.map((item) => (
 								<li
 									key={item.id}
-									className={`font-sourceCodePro uppercase transition-colors hover:text-cyan-600 md:font-semibold ${item.onlyMobile ? "md:hidden" : ""}`}
+									className={`font-sourceCodePro font-semibold uppercase transition-colors hover:text-cyan-600 ${item.onlyMobile ? "md:hidden" : ""}`}
 								>
-									<a href={item.url} target='_blank' rel='noopener noreferrer'>
+									<a
+										onClick={toggleNavigation}
+										href={item.url}
+										target='_parent'
+										rel='noopener noreferrer'
+									>
 										{item.title}
 									</a>
 								</li>
@@ -26,9 +48,9 @@ const Navbar = () => {
 					<div className='flex items-center justify-center gap-2'>
 						<a
 							href='#signup'
-							target='_blank'
+							target='_parent'
 							rel='noopener noreferrer'
-							className='font-sourceCodePro text-p-5 hover:text-p-1 hidden uppercase transition-colors md:block md:font-semibold'
+							className='hidden font-sourceCodePro uppercase text-p-5 transition-colors hover:text-p-1 md:block md:font-semibold'
 						>
 							New Account
 						</a>
@@ -41,10 +63,11 @@ const Navbar = () => {
 						</button>
 					</div>
 					<button
+						onClick={handleMenuBtnClick}
 						type='button'
 						className='rounded-md bg-cyan-400 px-2.5 py-1 font-semibold hover:bg-cyan-500 hover:ring-2 hover:ring-cyan-300 md:hidden'
 					>
-						Menu
+						{openNavigation ? "Close" : "Menu"}
 					</button>
 				</div>
 			</MaxWidthContainer>
