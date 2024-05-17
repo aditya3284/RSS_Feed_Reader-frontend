c226ba4824c60073ced0990bbf23452e81818e31
PS Ad <78262269+aditya3284@users.noreply.github.com>
2024-05-17 17:23:51 +0530

    add Heading Component - UI

---
+++ src/components/ui/Heading.jsx
@@ -0,0 +1,27 @@
+import PropTypes from "prop-types";
+import Tagline from "./Tagline";
+
+const Heading = ({ className, tagline, title, titleId, body }) => {
+	const classes = `max-w-[50rem] mx-auto mb-12 lg:mb-20 text-center ${className}`;
+	return (
+		<div className={classes}>
+			{tagline && <Tagline className='mb-4'>{tagline}</Tagline>}
+			{title && (
+				<h2 id={titleId} className='h2 text-s-1'>
+					{title}
+				</h2>
+			)}
+			{body && <p className='body-2 mt-4 text-s-4'>{body}</p>}
+		</div>
+	);
+};
+
+Heading.propTypes = {
+	className: PropTypes.string,
+	tagline: PropTypes.string,
+	title: PropTypes.string,
+	titleId: PropTypes.string,
+	body: PropTypes.string,
+};
+
+export default Heading;
