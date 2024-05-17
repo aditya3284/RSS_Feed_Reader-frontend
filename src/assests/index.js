c28374ae3880d6d7eb3cfaf08a7ed69c5fb8c01d
PS Ad <78262269+aditya3284@users.noreply.github.com>
2024-05-17 17:30:59 +0530

    add icons for feature cards

--- src/assests/index.js
+++ src/assests/index.js
@@ -1,7 +1,23 @@
+//social-media-icons
 import discordBlack from "./socials/discord.svg";
 import facebook from "./socials/facebook.svg";
 import instagram from "./socials/instagram.svg";
 import telegram from "./socials/telegram.svg";
 import twitter from "./socials/twitter.svg";
+//features-icons-svg
+import benefit_Icon1 from "./features/icon1.svg";
+import benefit_Icon2 from "./features/icon2.svg";
+import benefit_Icon3 from "./features/icon3.svg";
+import benefit_Icon4 from "./features/icon4.svg";
 
-export { discordBlack, facebook, instagram, telegram, twitter };
+export {
+	benefit_Icon1,
+	benefit_Icon2,
+	benefit_Icon3,
+	benefit_Icon4,
+	discordBlack,
+	facebook,
+	instagram,
+	telegram,
+	twitter,
+};
