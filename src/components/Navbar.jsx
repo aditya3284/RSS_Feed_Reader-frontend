a96e48c352247a459f4ea284a48de2c9f6c32e2f
PS Ad <78262269+aditya3284@users.noreply.github.com>
2024-05-21 09:58:58 +0530

    add Logo, Button and Link to Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 import { navigation } from "../constants";
+import Logo from "./Logo";
 import MaxWidthContainer from "./MaxWidthContainer";
+import Button from "./ui/Button";
 
 const Navbar = () => {
 	const [openNavigation, setOpenNavigation] = useState(false);
@@ -16,59 +19,56 @@ const Navbar = () => {
 		setOpenNavigation(false);
 	};
 	return (
-		<header className='fixed inset-x-0 top-0 z-10 h-14 w-full border-b border-s-6 bg-s-8/90 text-white backdrop-blur-lg transition-all'>
+		<header className='sticky inset-x-0 top-0 z-10 w-full border-b  border-s-6 bg-s-8/90 py-0.5 text-white backdrop-blur-2xl'>
 			<MaxWidthContainer>
-				<div className='font-so flex h-full items-center justify-between'>
-					<a href='/' className='z-20 font-sora font-bold'>
-						feed<span className='bg-cyan-600'>reader</span>
-					</a>
+				<div className=' my-5 flex h-full items-center justify-between font-sora'>
+					<Logo />
 					<nav
-						className={`md:flex ${openNavigation ? "fixed inset-x-0 inset-y-full z-10 h-svh w-svw bg-s-5/50 backdrop-blur-sm" : "hidden"}`}
+						aria-label='primary'
+						className={` md:flex ${openNavigation ? "fixed inset-x-0 inset-y-full z-10 h-svh w-svw bg-s-5/50 backdrop-blur-sm" : "hidden"}`}
 					>
 						<ol
-							className={`flex items-center gap-6 ${openNavigation ? "h-full w-full flex-col justify-center" : ""}`}
+							className={`flex items-center gap-4 lg:gap-10 ${openNavigation ? "h-full w-full flex-col justify-center" : ""}`}
 						>
 							{navigation.map((item) => (
 								<li
 									key={item.id}
 									className={`font-sourceCodePro font-semibold uppercase transition-colors hover:text-cyan-600 ${item.onlyMobile ? "md:hidden" : ""}`}
 								>
-									<a
+									<Link
 										onClick={toggleNavigation}
-										href={item.url}
-										target='_parent'
+										to={item.url}
 										rel='noopener noreferrer'
 									>
 										{item.title}
-									</a>
+									</Link>
 								</li>
 							))}
 						</ol>
 					</nav>
-					<div className='flex items-center justify-center gap-2'>
-						<a
-							href='#signup'
-							target='_parent'
-							rel='noopener noreferrer'
-							className='hidden font-sourceCodePro uppercase text-p-5 transition-colors hover:text-p-1 md:block md:font-semibold'
+					<div className='flex gap-3'>
+						<div className='flex items-center justify-center gap-3 '>
+							<Link
+								to='/signup'
+								rel='noopener noreferrer'
+								className='font-sourceCodePro uppercase text-p-5 transition-colors hover:text-p-1 md:font-semibold'
+							>
+								New Account
+							</Link>
+							<Button
+								href='/login'
+								className='bg-p-5 px-2 py-1 uppercase tracking-tight text-s-7 hover:text-s-1 hover:ring-2 hover:ring-p-6 dark:bg-s-1 dark:hover:text-p-1'
+							>
+								Sign in
+							</Button>
+						</div>
+						<Button
+							onClickFn={handleMenuBtnClick}
+							className='px-1.5 py-1 text-s-1 hover:ring-2 hover:ring-p-6 md:hidden'
 						>
-							New Account
-						</a>
-						<button
-							type='button'
-							href='#login'
-							className='hidden rounded-md bg-cyan-400 px-2.5 py-1 font-semibold transition-all hover:bg-cyan-500 hover:ring-2 hover:ring-cyan-300 md:block'
-						>
-							Sign in
-						</button>
+							{openNavigation ? "Close" : "Menu"}
+						</Button>
 					</div>
-					<button
-						onClick={handleMenuBtnClick}
-						type='button'
-						className='rounded-md bg-cyan-400 px-2.5 py-1 font-semibold hover:bg-cyan-500 hover:ring-2 hover:ring-cyan-300 md:hidden'
-					>
-						{openNavigation ? "Close" : "Menu"}
-					</button>
 				</div>
 			</MaxWidthContainer>
 		</header>
