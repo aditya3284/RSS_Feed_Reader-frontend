2d9c22537b19d4d9680bde4a952f14c25c6374a1
PS Ad <78262269+aditya3284@users.noreply.github.com>
2024-05-22 06:27:16 +0530

    add theme handling for ThemeToggle componenet

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 function update() {
 	if (
 		localStorage.getItem("theme") === "dark" ||
@@ -8,4 +10,50 @@ function update() {
 	} else {
 		document.documentElement.classList.remove("dark");
 	}
+}
+
+function useTheme() {
+	const [setting, setSetting] = useState(null);
+
+	function setThemeFromLocalStorage() {
+		let theme = localStorage.getItem("theme");
+		if (theme === "light" || theme === "dark") {
+			setSetting(theme);
+		} else {
+			setSetting(
+				window.matchMedia("(prefers-colors-scheme: dark)").matches
+					? "dark"
+					: "light"
+			);
+		}
+	}
+
+	// set theme on mount
+	useEffect(() => {
+		setThemeFromLocalStorage();
+		let mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+		mediaQuery.addEventListener("change", update);
+
+		const handleStorageChange = () => {
+			update();
+			setThemeFromLocalStorage();
+		};
+
+		window.addEventListener("storage", handleStorageChange);
+
+		return () => {
+			mediaQuery.removeEventListener("change", update);
+			window.removeEventListener("storage", handleStorageChange);
+		};
+	}, []);
+
+	//to handle the theme after mounting
+	useEffect(() => {
+		if (setting === "light" || setting === "dark") {
+			localStorage.setItem("theme", setting);
+		}
+		update();
+	}, [setting]);
+
+	return { setting, setSetting };
 }
