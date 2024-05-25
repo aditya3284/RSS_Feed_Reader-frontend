c2f60209bcec351f4d47c96e6034d8a45f40dcff
PS Ad <78262269+aditya3284@users.noreply.github.com>
2024-05-25 13:20:20 +0530

    add client side routiing using react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,15 @@
-import Footer from "./components/Footer";
-import Navbar from "./components/Navbar";
-import Features from "./components/sections/Features";
-import Hero from "./components/sections/Hero";
-import Integrations from "./components/sections/Integrations";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Home, NotFound404 } from "./pages";
 
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <Home />,
+		errorElement: <NotFound404 />,
+	},
+]);
 const App = () => {
-	return (
-		<>
-			<Navbar />
-			<main className=''>
-				<Hero />
-				<Features />
-				<Integrations />
-				<section id='pricing' className='padding bg-p-6'>
-					Special Offer
-				</section>
-				<section id='offer' className='padding bg-s-3'>
-					customer Review
-				</section>
-				<section id='how-to-use' className='padding'>
-					Newsletter
-				</section>
-				<section id='roadmap' className='padding bg-black text-white'>
-					footer
-				</section>
-				<Footer />
-			</main>
-		</>
-	);
+	return <RouterProvider router={router} />;
 };
 
 export default App;
