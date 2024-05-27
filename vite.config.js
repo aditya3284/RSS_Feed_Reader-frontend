d12a3c698b18126f773d866b607942f7701cf4f9
PS Ad <78262269+aditya3284@users.noreply.github.com>
2024-05-27 09:38:28 +0530

    configure vite proxy server

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	server: { proxy: { "/api/v1": { target: "http://localhost:3737" } } },
+});
