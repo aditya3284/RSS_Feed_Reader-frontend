a2e6371dc27fcdfd12a32efb4162e6b62a8c6c08
PS Ad <78262269+aditya3284@users.noreply.github.com>
2024-06-24 15:57:11 +0530

    create recent dashboard component

--- src/pages/dashboard/Recent.jsx
+++ src/pages/dashboard/Recent.jsx
@@ -1,5 +1,55 @@
+import Button from "../../components/ui/Button";
+import {
+	DashboardCard,
+	DashboardCardContent,
+	DashboardCardDescription,
+	DashboardCardHeader,
+	DashboardCardTitle,
+} from "../../components/ui/DashboardCard";
+import { dummyFeedItemData } from "../../constants";
+
 const Recent = () => {
-	return <div>recent</div>;
+	document.querySelector("title").text = "Recent";
+
+	return (
+		<div className='p-5'>
+			<div className=''>
+				<h1 className='mb-5 text-3xl font-bold text-s-8 dark:text-s-1'>
+					Recent Uploads
+				</h1>
+				<ul className='grid gap-5 md:grid-cols-2'>
+					{dummyFeedItemData.map(({ _id, thumbnailUrl, title, content }) => (
+						<li key={_id}>
+							<DashboardCard className='grid bg-s-2 lg:grid-cols-2 dark:bg-s-6'>
+								<DashboardCardHeader
+									feedItemID={_id}
+									className='grid place-items-center'
+								>
+									<img
+										src={thumbnailUrl}
+										alt=''
+										width={480}
+										height={360}
+										className='aspect-video w-full bg-s-4 object-cover'
+										loading='lazy'
+									/>
+								</DashboardCardHeader>
+								<DashboardCardContent feedItemID={_id} className='py-8'>
+									<DashboardCardTitle className='line-clamp-1'>
+										{title}
+									</DashboardCardTitle>
+									<DashboardCardDescription className='line-clamp-2 text-s-8 dark:text-s-3'>
+										{content}
+									</DashboardCardDescription>
+								</DashboardCardContent>
+							</DashboardCard>
+						</li>
+					))}
+				</ul>
+				<Button>Load More</Button>
+			</div>
+		</div>
+	);
 };
 
 export default Recent;
