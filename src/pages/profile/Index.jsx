ab388ea71911384262c071a418452e8d92952ae6
PS Ad <78262269+aditya3284@users.noreply.github.com>
2024-07-15 13:33:07 +0530

    create profile index and notification page

---
+++ src/pages/profile/Index.jsx
@@ -0,0 +1,9 @@
+const Index = () => {
+	return (
+		<div className='grid h-dvh place-items-center dark:text-s-2 '>
+			<h1 className='text-2xl font-extrabold uppercase sm:text-4xl'>Profile</h1>
+		</div>
+	);
+};
+
+export default Index;
---
+++ src/pages/profile/Notification.jsx
@@ -0,0 +1,7 @@
+import MaxWidthContainer from "../../components/MaxWidthContainer";
+
+const Notification = () => {
+	return <MaxWidthContainer>notifications</MaxWidthContainer>;
+};
+
+export default Notification;
