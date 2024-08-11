a58e7f8734e26a50c24962e9d7712b1ff53a7674
PS Ad <78262269+aditya3284@users.noreply.github.com>
2024-08-11 09:11:53 +0530

    handle logout for profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,29 +1,27 @@
 import { NavLink, Outlet } from "react-router-dom";
-import { Exit } from "../assests";
+import { ExitIcon } from "../assests";
 import Logo from "../components/Logo";
 import SecondarySideNav from "../components/SecondarySideNav";
 import Sidebar from "../components/Sidebar";
 import ThemeToggle from "../components/ThemeToggle";
+import Button from "../components/ui/Button";
 import { profileSideNav } from "../constants";
-import useLogOut from "../hooks/useLogOut";
 import SettingLayout from "../layouts/SidebarLayout";
 
 const Dashboard = () => {
-	const { logOut } = useLogOut();
-
 	return (
 		<SettingLayout list={profileSideNav}>
 			<Sidebar>
 				<Logo className='px-4' />
 				<SecondarySideNav>
 					<ul className='pb-[23.5rem] text-s-7'>
-						{profileSideNav.map(({ id, label, link }) => (
-							<li key={id} className='my-1'>
+						{profileSideNav.map(({ id, label, link, onlyMobile }) => (
+							<li key={id} className={`my-1 ${onlyMobile && "md:hidden"}`}>
 								<NavLink
 									to={link}
 									className={({ isActive }) =>
 										isActive
-											? "bg-s-2  text-cyan-600 hover:bg-[#d0d0d0] dark:bg-s-8 dark:text-p-5 dark:hover:bg-s-11"
+											? "bg-s-2 text-cyan-600 hover:bg-[#d0d0d0] dark:bg-s-8 dark:text-p-5 dark:hover:bg-s-11"
 											: "text-s-8  hover:bg-s-2/60 dark:text-s-3 dark:hover:bg-s-6"
 									}
 								>
@@ -36,14 +34,14 @@ const Dashboard = () => {
 							</li>
 						))}
 					</ul>
-					<button
+					<Button
 						className='flex w-full gap-5 rounded-xl px-5 py-4 font-bold hover:bg-s-2/60 dark:text-s-3  dark:hover:bg-s-8'
-						onClick={logOut}
+						href={"/logout"}
 						title='logout'
 					>
-						<img src={Exit} width={24} loading='lazy' alt='logout img' />
-						LogOut
-					</button>
+						<ExitIcon />
+						logOut
+					</Button>
 					<ThemeToggle
 						showText={true}
 						classes={`rounded-xl px-5 py-4 w-full hover:bg-s-2/60 dark:text-s-3  dark:hover:bg-s-8`}
