ef13fe12870e5566b0a40fa2072997cd2dc38e7d
PS Ad <78262269+aditya3284@users.noreply.github.com>
2024-08-11 10:55:21 +0530

    create Pricing page content

--- src/pages/Pricing.jsx
+++ src/pages/Pricing.jsx
@@ -1,12 +1,17 @@
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
+import FaqAccordion from "../components/sections/FaqAccordion";
+import PricingPlan from "../components/sections/PricingPlan";
+
+import { frequentlyAskedQuestions, pricing } from "../constants";
 
 const Pricing = () => {
 	return (
 		<>
 			<main>
 				<Navbar />
-				<h1>Hello from Pricing Page</h1>
+				<PricingPlan list={pricing} />
+				<FaqAccordion list={frequentlyAskedQuestions} />
 			</main>
 			<Footer />
 		</>
