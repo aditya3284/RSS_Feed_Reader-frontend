fc2b3eba9960b33a50df9520f052bf42479fe54a
PS Ad <78262269+aditya3284@users.noreply.github.com>
2024-08-11 09:17:29 +0530

    add smooth open and close transistion for EditableDetails

--- src/components/EditableDetails.jsx
+++ src/components/EditableDetails.jsx
@@ -2,26 +2,35 @@ import PropTypes from "prop-types";
 import { useState } from "react";
 import Button from "./ui/Button";
 
-const EditableDetails = ({ label, description, children }) => {
+const EditableDetails = ({ label, description, children, labelHtmlFor }) => {
 	const [isEditing, setIsEditing] = useState(false);
 
 	const handleClick = () => {
 		isEditing ? setIsEditing(false) : setIsEditing(true);
 	};
+
 	return (
 		<section aria-labelledby='profile-name' className='border-s-5 py-3'>
 			<div className='mb-2 flex justify-between'>
 				<div className=''>
-					<h3 className='text-xl font-semibold' id='profile-name'>
+					<label
+						htmlFor={labelHtmlFor}
+						className='text-xl font-semibold'
+						id='profile-name'
+					>
 						{label}
-					</h3>
+					</label>
 					<p>{description}</p>
 				</div>
 				<Button onClickFn={handleClick} className='hover:underline'>
 					{isEditing ? "Cancel" : "Edit"}
 				</Button>
 			</div>
-			<div className={isEditing ? "" : "hidden"}>{children}</div>
+			<div
+				className={`grid transition-all  duration-500 ${isEditing ? "grid-rows-[1fr]" : "grid-rows-[0fr]"}`}
+			>
+				<div className='overflow-hidden'>{children}</div>
+			</div>
 		</section>
 	);
 };
@@ -29,6 +38,7 @@ const EditableDetails = ({ label, description, children }) => {
 EditableDetails.propTypes = {
 	label: PropTypes.string,
 	description: PropTypes.string,
+	labelHtmlFor: PropTypes.string,
 	children: PropTypes.node,
 };
 
